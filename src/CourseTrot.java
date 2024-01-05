e397b1767595af1c55903565b0209d5268da0b26
Claire AVILA <avila.claire@gmail.com>
2024-01-05 15:43:31 +0100

    ajout du tiercé

--- src/CourseTrot.java
+++ src/CourseTrot.java
@@ -1,15 +1,17 @@
-
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 
 public class CourseTrot {
 
-	public static void main(String[] args) { //problème d'intentation ? 
+	public static void main(String[] args) {
 
 	}
 
 
 		// Initialiser le tableau des chevaux
 		public static int[][] newHorses() {
-			int[][] horses = new int[20][2];
+			int[][] horses = new int[20][12];
 			for (int i = 0; i < horses.length; i++) {
 			}
 			return horses;
@@ -60,16 +62,29 @@ public class CourseTrot {
 		//déclarer les 3 vainqueurs
 		public static boolean finishRace(int[][] horses) {
 			return false;
-
+// si le cheval fait - 2400m alors il a perdu
 			
 		}
 
 		//etat actuel de la course à chaque tours
 		public static void currentRace(int[][] horses) {
+			// donne l'état actuel de la course de chaque cheval ainsi que les tours
 		}
 
 		//résultat de la course
 		public static void showResult (int[][] horses) {
+			//montre les 3 vainqueurs
+			public static Set <Integer> horseRace() {
+				Random random = new Random();
+			// le set est une collection qui permet de ne pas faire de doublon on s'assure que chaque numéro de cheval est unique
+				Set < Integer> horse = new HashSet<>();//HashSet stocke des éléments et permet d'ajouter et de suprimer ce qui a déjà été suggéré par la méthode random
+			//selectionner 3 chevaux
+				while (horse.size() <3) {
+					int randomNumber =random.nextInt(20)+1;
+					horse.add(randomNumber);
+						
+				}
+				return horse;
 
 		}
 }
